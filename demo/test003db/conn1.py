8107518bd322ba1baf3f026fdab0adbf4c242a71
Cheng <lechenggood@foxmail.com>
2018-08-08 22:58:11 +0800

    mysql

--- demo/test003db/conn1.py
+++ demo/test003db/conn1.py
@@ -3,4 +3,4 @@ import pymysql
 
 # pip install paramiko完美结决pymysql问题
 def getConn():
-    return pymysql.connect("localhost", "root", "", "test")
+    return pymysql.connect("localhost", "root", "123456", "other_db")
--- demo/test003db/test1.py
+++ demo/test003db/test1.py
@@ -45,17 +45,44 @@ print("Database version : %s " % data)
 cursor = db.cursor()
 
 # SQL 插入语句
-sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
-         LAST_NAME, AGE, SEX, INCOME)
-         VALUES ('Mac', 'Mohan222', 20, 'M', 2000)"""
+# sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+#          LAST_NAME, AGE, SEX, INCOME)
+#          VALUES ('Mac', 'chmiao', 22, 'M', 1234)"""
+# try:
+#     # 执行sql语句
+#     cursor.execute(sql)
+#     # 提交到数据库执行
+#     db.commit()
+# except:
+#     # 如果发生错误则回滚
+#     db.rollback()
+#
+# # 关闭数据库连接
+# db.close()
+
+# 查询操作
+# 使用cursor()方法获取操作游标
+cursor = db.cursor()
+
+# SQL 查询语句
+sql = "SELECT * FROM EMPLOYEE \
+       WHERE INCOME > '%d'" % (1000)
 try:
-    # 执行sql语句
+    # 执行SQL语句
     cursor.execute(sql)
-    # 提交到数据库执行
-    db.commit()
+    # 获取所有记录列表
+    results = cursor.fetchall()
+    for row in results:
+        fname = row[0]
+        lname = row[1]
+        age = row[2]
+        sex = row[3]
+        income = row[4]
+        # 打印结果
+        print ("fname=%s,lname=%s,age=%d,sex=%s,income=%d" % \
+               (fname, lname, age, sex, income ))
 except:
-    # 如果发生错误则回滚
-    db.rollback()
+    print ("Error: unable to fetch data")
 
 # 关闭数据库连接
 db.close()
